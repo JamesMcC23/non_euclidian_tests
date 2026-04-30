4fc4cd53a934d3492aca311a03da5c972da2bb50
JamesMcC23 <McCullough-J28@ulster.ac.uk>
2026-04-30 16:25:37 +0100

    tweaks to portal code

--- non_euclidean_test/Assets/code/portal_script.cs
+++ non_euclidean_test/Assets/code/portal_script.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class portal_script : MonoBehaviour
 {
-    [SerializeField] Gameobject linked_portal;
+    [SerializeField] GameObject linked_portal;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    private OnCollisionEnter(Collision other)
+    void OnCollisionEnter (Collider other)
     {
         if(other.CompareTag("player"))
         {
             other.transform.position = linked_portal.transform.position;
-            other.transform.rotation.y = linked_portal.transform.rotation.z;
+            //other.transform.rotation.y = linked_portal.transform.rotation.z;
         }
 
     }
